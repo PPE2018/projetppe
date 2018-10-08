0892155640407b2c48bf0de9ee8efb2d2a9c989a
Loan <jouffroyloan@gmail.com>
2018-10-08 15:44:37 +0200

    Debut ajout

--- creer_offre.php
+++ creer_offre.php
@@ -1,10 +1,10 @@
 <html>
 <head>
-    <title></title>
+    <title>Bootstrap template</title>
     <meta charset="utf-8">
     <link href="style/style.css" rel="stylesheet">
 </head>
 <body>
-  <h1>
+
 </body>
 </html>
