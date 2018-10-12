e780fe65af4a755d33c68f1a0aeefa26fd7fac21
Amandine <amande038@gmail.com>
2018-10-12 17:41:42 +0200

    création de la page offre d'emplois

---
+++ consultation_offre_rh.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Consultation des offres  :</title>
+        <meta charset="UTF-8">
+        <link href="creation_offre.css" rel="stylesheet" type="text/css" media="screen">
+    </head>
+    <body>
+      <h1> Consultation des offres: </h1>
+
+
+    </body>
