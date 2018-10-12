98ee3578d92df70874511fd00d8ca818df37b5ce
Amandine <amande038@gmail.com>
2018-10-12 16:25:51 +0200

    Merge branch 'master' of https://github.com/PPE2018/projetppe ajout du type d'offre

--- creation_offre.php
+++ creation_offre.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>Création_Offre</title>
+        <title>Création_Offre :</title>
         <meta charset="UTF-8">
         <link href="creation_offre.css" rel="stylesheet" type="text/css" media="screen">
     </head>
@@ -10,13 +10,12 @@
         <li><a href="#news">Consultation des offres</a></li>
         <li><a href="#contact">Création des offres</a></li>
         <li><a href="#about">Réception des candidatures</a></li>
-        <li style="float:right"><a class="active" href="#about">Français</a></li>
-        <li style="float:right"><a class="active" href="#about">Anglais</a></li>
         <li style="float:right"><a class="active" href="#about">Mon compte</a></li>
+        <li style="float:right" ><a class="active" href="#">Langue</a></li>
       </ul>
 
       <form action="creation_offre.php" method="post">
-        <h1> Création de l'offre </h1>
+        <h1> Création de l'offre : </h1>
 
       <!-- Conteneur du formulaire -->
 
@@ -26,13 +25,24 @@
 
           <div>
             <h3> Intitulé du post :</h3>
-              <input type="text" name="nom_post">
+              <input type="text" name="nom_post" required="required" value="">
           </div>
+
+          <!-- Saisie du type post-->
+
+        <div>
+          <h3>Type de l'offre :</h3>
+
+          <input type="checkbox" name="doc_cv" required="required" />CDI
+          <input type="checkbox" name="doc_lm" required="required" />CDD
+          <input type="checkbox" name="doc_video" required="required" />Intérim
+        </div>
 
           <!-- Saisie Compétences-->
 
           <div>
             <h3> Compétences :</h3>
+              <input type="submit" name="soumettre" value="+"/>
             <select name="Compétence">
               <option value="Compétence_1">Compétence_1</option>
               <option value="Compétence_2">Compétence_2</option>
@@ -45,7 +55,7 @@
 
           <div>
             <h3>Lieu :</h3>
-              <input type="text" name="nom_lieu" >
+              <input type="text" name="nom_lieu" required="required" value="" />
           </div>
 
             <!-- Saisie de la date limite de dépots-->
@@ -53,7 +63,7 @@
           <div>
             <h3> Date limite de dépots :</h3>
 
-            <input type="date" name="date_depots" >
+            <input type="date" name="date_depots" required="required" >
           </div>
 
           <!-- Saisie documents a fournir (cases à cocher)-->
@@ -61,7 +71,9 @@
         <div>
           <h3>Documents à fournir :</h3>
 
-          <input type="text" name="nom_docsr" >
+          <input type="checkbox" name="doc_cv" required="required" />CV
+          <input type="checkbox" name="doc_lm" required="required" />Lettre motivation
+          <input type="checkbox" name="doc_video" required="required" />Vidéo
         </div>
 
         <!-- Saisie vidéo en url-->
@@ -69,7 +81,7 @@
       <div>
         <h3>Vidéos :</h3>
 
-        <input type="text" name="" >
+        <input type="url" name="url" required="required" value="" />
       </div>
 
           <div>
