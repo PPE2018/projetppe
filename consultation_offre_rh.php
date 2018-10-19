b761a58e905ae3a64cde4593700dc556d1279312
Amandine <amande038@gmail.com>
2018-10-19 16:32:03 +0200

    modification

--- consultation_offre_rh.php
+++ consultation_offre_rh.php
@@ -2,9 +2,44 @@
     <head>
         <title>Consultation des offres  :</title>
         <meta charset="UTF-8">
-        <link href="creation_offre.css" rel="stylesheet" type="text/css" media="screen">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
+      <body>
+          <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="#">Accueil</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarsExample05">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="#">Consultation des offres</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Création des offres</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Réception des candidatures</a>
+              </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Langue</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown05">
+                  <a class="dropdown-item" href="#">Français</a>
+                  <a class="dropdown-item" href="#">English</a>
+                </div>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Mon Compte</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
       <h1> Consultation des offres: </h1>
 
 
--- creation_candidature.php
+++ creation_candidature.php
@@ -2,7 +2,9 @@
     <head>
         <title>Création_Candidature :</title>
         <meta charset="UTF-8">
-        <link href="creation_offre.css" rel="stylesheet" type="text/css" media="screen">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
     </head>
     <body>
 
