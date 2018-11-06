fad7d105ce831c0c192d7d4c13812831a76876f6
Loan <jouffroyloan@gmail.com>
2018-11-06 17:39:50 +0100

    tdet

--- envois/creer_offre_envois.php
+++ envois/creer_offre_envois.php
@@ -8,42 +8,28 @@
     <?php
         include '../bdd/bdd.php';
 
-
-        //// TODO: Recuperer les données du formulaire via post
-
         if(isset($_POST['date_depots'])){
             $dateLimite = $_POST['date_depots'];
-            echo $dateLimite;
-            echo '<br />';
         }
 
-        //// TODO: FAIRE DES ATTRIBUTS DANS BDD : Lieu, Type de contrat, et vrai decription
         if(isset($_POST['description'])){
             $description = $_POST['description'];
-            echo $description;
-            echo '<br />';
         }
 
         if(isset($_POST['lieu'])){
             $lieu = $_POST['lieu'];
-            echo $lieu;
-            echo '<br />';
         }
 
         if(isset($_POST['salaire'])){
             $salaire = $_POST['salaire'];
-            echo $salaire;
-            echo '<br />';
         }
 
         if(isset($_POST['contrat'])){
             $contrat = $_POST['contrat'];
-            echo $contrat;
-            echo '<br />';
         }
 
         //id du RH
-        $idPersonne = 4;
+        $idPersonne = 4; //A CHANGGGGGGEEEEERRRR
         if(isset($_POST['id_personne'])){
             $idPersonne = $_POST['id_personne'];
         }
@@ -51,25 +37,19 @@
         //Titre
         if(isset($_POST['nom_post'])){
             $lib = $_POST['nom_post'];
-            echo $lib;
-            echo '<br />';
         }
 
         if(isset($_POST['url'])){
             $video = $_POST['url'];
-            echo $video;
-            echo '<br />';
         }
 
         //TODO
         //id_personne dependra de la session de l'utilisateur
         $requete = "INSERT INTO offre_emplois(date_limite, description, id_personne, libelle, video, lieu, salaire, type_contrat) VALUES ('$dateLimite', '$description', $idPersonne, '$lib', '$video', '$lieu', $salaire, '$contrat')";
-        echo $requete;
         $resultat = mysqli_query($connexion, $requete);
 
         if(isset($_POST['doc_cv'])){
             $doc_cv = true;
-            echo '<br />';
         }
         else{
           $doc_cv = false;
@@ -77,7 +57,6 @@
 
         if(isset($_POST['doc_lm'])){
             $doc_lm = true;
-            echo '<br />';
         }
         else{
           $doc_lm = false;
@@ -85,7 +64,6 @@
 
         if(isset($_POST['doc_video'])){
             $doc_video = true;
-            echo '<br />';
         }
         else{
           $doc_video = false;
@@ -123,7 +101,7 @@
         else{
             echo 'L\'offre d\'emplois a bien été créer !<br />';
         }
-        echo '<a href="../ajout.php">Retour</a>';
+        echo '<a href="../creation_offre.php">Retour</a>';
     ?>
 </body>
 </html>
