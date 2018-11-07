81abcd5eb4f067311ad3a40cbdb3a7de20245751
Amandine <amande038@gmail.com>
2018-11-07 17:36:40 +0100

    mis un peu de bootstrap mais à perfectionner dans consulattion offre.

--- consultation_offre_rh.php
+++ consultation_offre_rh.php
@@ -1,20 +1,20 @@
-<<?php
+<?php
 
 $link = mysqli_init();     // initialiser et connection au serveur
 
-$connection = mysqli_connect('localhost', 'root', ''); /* savoir si la connection au serveur de base de données*/
+ $connection = mysqli_connect('localhost', 'root', ''); /* savoir si la connection au serveur de base de données*/
 if($connection){
-  echo 'Connexion au serveur réussie <br/>';
+  //echo 'Connexion au serveur réussie <br/>';
   $ok = mysqli_select_db($connection, 'ppe');
   if($ok){
-    echo'Base de données selectionner';
+    //echo'Base de données selectionner';
   }
   else{
-    echo 'Echec de la sélection de la base';
+    //echo 'Echec de la sélection de la base';
   }
 }
 else {
-  echo ' Erreur de la connection';
+  //echo ' Erreur de la connection';
 }
 
  ?>
@@ -26,9 +26,9 @@ else {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <link href="style/consultation_offre_rh.css" rel="stylesheet">
     </head>
     <body>
-      <body>
           <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
           <a class="navbar-brand" href="#">Accueil</a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
@@ -37,10 +37,10 @@ else {
 
           <div class="collapse navbar-collapse" id="navbarsExample05">
             <ul class="navbar-nav mr-auto">
-              <li class="nav-item">
+              <li class="nav-item active">
                 <a class="nav-link" href="consultation_offre_rh.php">Consultation des offres</a>
               </li>
-              <li class="nav-item active">
+              <li class="nav-item ">
                 <a class="nav-link" href="creation_offre.php">Création des offres</a>
               </li>
               <li class="nav-item">
@@ -62,32 +62,43 @@ else {
           </div>
         </nav>
 
-        <?php
-        $resultat=mysqli_query($connection, 'SELECT*  FROM offre_emplois'); /*permet d'afficher les données*/
-        while($ligne = mysqli_fetch_array($resultat, MYSQLI_BOTH)){
-          $id=$ligne['id_offre'];
-          $libelle = $ligne['libelle'];
-          $desc=$ligne['description'];
-          $lieu=$ligne['lieu'];
-          $typecontr=$ligne['type_contrat'];
-          $salaire= $ligne['salaire'];
-          $datelim=$ligne['date_limite'];
-          $video=$ligne['video'];
+        <div class="container">
+          <?php
 
-          echo 'Consultation des offres: '.$id.'<br/> Nom emploi : '.$libelle.'<br/> Descriptif :'. $desc.'<br/> Lieu :'.$lieu.'<br/> type de contrat :'.$typecontr.'<br/>Salaire :'.$salaire.'<br/> Date limite :'.$datelim.'<br/> URL de la vidéo :'.$video.'<br/>';
+          $resultat=mysqli_query($connection, 'SELECT*  FROM offre_emplois'); /*permet d'afficher les données*/
+          while($ligne = mysqli_fetch_array($resultat, MYSQLI_BOTH))
+          {
+            $id=$ligne['id_offre'];
+            $libelle = $ligne['libelle'];
+            $desc=$ligne['description'];
+            $lieu=$ligne['lieu'];
+            $typecontr=$ligne['type_contrat'];
+            $salaire= $ligne['salaire'];
+            $datelim=$ligne['date_limite'];
+            $video=$ligne['video'];
 
-        }
+            ?>
+            <div class="card bg-secondary text-white">
 
+              <div class="card-body">
+                <?php
+                  echo 'Consultation des offres : '.$id.'<br/> Nom emploi : '.$libelle.'<br/> Descriptif : '. $desc.'<br/> Lieu : '.$lieu.'<br/> type de contrat : '.$typecontr.'<br/>Salaire : '.$salaire.'<br/> Date limite : '.$datelim.'<br/> URL de la vidéo : '.$video.'<br/> <br/><br/>';
+                 ?>
+               </div>
+               <a href="#" class="btn btn-info" role="button">Réception de candidatures</a>
+               <a href="creation_offre.php" class="btn btn-info" role="button">Modifier l'offre</a>
+               <input type="submit" class="btn btn-info" value="Supprimer l'offre">
+             </div>
+          <?php
+           }
 
-         ?>
+          ?>
 
-
-
-
-
+          <br>
+      </div>
 
     </body>
-<<?php
+  </html>
+<?php
 mysqli_close($connection);
-
  ?>
