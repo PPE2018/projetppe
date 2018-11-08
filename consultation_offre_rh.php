3683f9004528bdcb6e4c134e71eb5a5dd60b1329
Amandine <amande038@gmail.com>
2018-11-08 20:20:30 +0100

    Modification final de consultation offre

--- consultation_offre_rh.php
+++ consultation_offre_rh.php
@@ -41,49 +41,29 @@
           </div>
         </nav>
 
-        <div class="container">
-          <div class="row">
+        <div class="container-fluid">
+          <div class="row content">
+            <div class='col-sm-4'>
+            </div>
+            <div classe='col-sm-4'>
               <?php
               include 'bdd/bdd.php';
-              $resultat=mysqli_query($connexion, 'SELECT offre_emplois.libelle as libelle_offre, description, lieu, type_contrat, salaire, date_limite, video, competence.libelle FROM offre_emplois INNER JOIN posseder ON offre_emplois.id_offre = posseder.id_offre INNER JOIN competence ON posseder.id_competence = competence.id_competence ORDER BY offre_emplois.libelle'); /*permet d'afficher les données*/
-              $ligne = mysqli_fetch_array($resultat);
-              $libelle = $ligne['libelle_offre'];
-              $desc=$ligne['description'];
-              $lieu=$ligne['lieu'];
-              $typecontr=$ligne['type_contrat'];
-              $salaire= $ligne['salaire'];
-              $datelim=$ligne['date_limite'];
-              $video=$ligne['video'];
-              $competence = $ligne['libelle'];
-              echo"<div class='col-sm-8'>
-                      <div class='card bg-secondary text-white'>
-                        <div class='card-body'>
-                          $libelle<br/>
-                        </div>
-                        <div classe='card-body1'>
-                          $desc
-                          <br/> Lieu : $lieu
-                          <br/> Type de contrat : $typecontr
-                          <br/> Salaire annuel en euros : $salaire
-                          <br/> Date limite : $datelim
-                          <br/> URL de la vidéo : $video
-                          <br/> Compétences : $competence";
-              while($ligne = mysqli_fetch_array($resultat))
-              {
-                if($libelle != $ligne['libelle_offre']){
-                  echo '</div></div></div>';
-                  echo "
-                  <div class='container1'>
-                    <div class='row'>
-                      <div class='col-sm-4'>
-                        <div class='btn-group-vertical'>
-                          <a href='#' class='btn btn-info' role='button'>Réception de candidatures</a>
-                          <a href='creation_offre.php' class='btn btn-info' role='button'>Modifier</a>
-                          <input type='submit' class='btn btn-info' value='Supprimer'>
-                        </div>
-                      </div>
-                    </div>
-                  </div>";
+              $resultat=mysqli_query($connexion, 'SELECT offre_emplois.id_offre, offre_emplois.libelle as libelle_offre, description, lieu, type_contrat, salaire, date_limite, video, competence.libelle FROM offre_emplois INNER JOIN posseder ON offre_emplois.id_offre = posseder.id_offre INNER JOIN competence ON posseder.id_competence = competence.id_competence ORDER BY offre_emplois.libelle'); /*permet d'afficher les données*/
+
+              $id = -1; //index impossible
+              while($ligne = mysqli_fetch_array($resultat, MYSQLI_BOTH)){
+                $id_offre=$ligne['id_offre'];
+                if($id_offre!=$id){ // si id est différents de l'autre id
+                  if ($id!=-1) {
+                    // on affiche le bouton
+                    echo "</div>
+                          <div class='btn-group text-center'>
+                            <button type='button' class='btn btn-light'>Modifier</button>
+                            <button type='button' class='btn btn-dark'>Réception candidatures</button>
+                          </div>
+
+                          </div>";
+                  }
                   $libelle = $ligne['libelle_offre'];
                   $desc=$ligne['description'];
                   $lieu=$ligne['lieu'];
@@ -92,45 +72,42 @@
                   $datelim=$ligne['date_limite'];
                   $video=$ligne['video'];
                   $competence = $ligne['libelle'];
-                  echo"<div class='col-sm-8'>
+
+                  echo "
                           <div class='card bg-secondary text-white'>
                             <div class='card-body'>
                               $libelle<br/>
                             </div>
                             <div classe='card-body1'>
-                              $desc
+                              <p>$desc
                               <br/> Lieu : $lieu
                               <br/> Type de contrat : $typecontr
                               <br/> Salaire annuel en euros : $salaire
                               <br/> Date limite : $datelim
-                              <br/> URL de la vidéo : $video
-                              <br/> Compétences :
-                              <br /> - $competence";
+                              <br/> URL de la vidéo : $video;
+                              <br/> Compétences : <br/> - $competence</p>";
                 }
                 else{
-                  echo "<br />-".$ligne['libelle'];
-                  $libelle = $ligne['libelle_offre'];
-                  echo '<br/>
-                  <br/>
-                </div>
-              </div>';
+                  $competence = $ligne['libelle'];
+                  echo "<br/> - $competence";
+
                 }
-             }
-             echo "
-             <div class='container1'>
-               <div class='row'>
-                 <div class='col-sm-4'>
-                   <div class='btn-group-vertical'>
-                     <a href='#' class='btn btn-info' role='button'>Réception de candidatures</a>
-                     <a href='creation_offre.php' class='btn btn-info' role='button'>Modifier</a>
-                     <input type='submit' class='btn btn-info' value='Supprimer'>
-                   </div>
-                 </div>
-               </div>
-             </div>";
-             ?>
-          </div>
-        </div>
+                $id=$id_offre;
+              }
+              echo "</div>
+                    <div class='btn-group '>
+                      <button type='button' class='btn btn-light'>Modifier</button>
+                      <button type='button' class='btn btn-dark'>Réception candidatures</button>
+                    </div>
+                    </div>";
+
+              ?>
+
+            </div>
+            <div class='col-sm-4'>
+            </div>
+           </div>
+         </div>
 
     </body>
     <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
