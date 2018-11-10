7d5461f74b9fdcb26416406b58c76230fec0134c
Amandine <amande038@gmail.com>
2018-11-10 12:40:44 +0100

    traduction francais anglais des pages création d'une offre et consulation offre

--- creation_offre.php
+++ creation_offre.php
@@ -8,82 +8,98 @@
         <link rel="stylesheet" href="style/style.css">
     </head>
     <body>
-        <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Accueil</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+      <?php
+
+      $langue = 'fr';
+
+      if (isset($_GET['langue']))
+        $langue = $_GET['langue'];
+      include "langue_".$langue.".php";
+
+      echo"
+        <nav class='navbar sticky-top navbar-expand-lg navbar-dark bg-dark'>
+        <a class='navbar-brand' href='#'>$str[1]</a>
+        <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarsExample05' aria-controls='navbarsExample05' aria-expanded='false' aria-label='Toggle navigation'>
+          <span class='navbar-toggler-icon'></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarsExample05">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="consultation_offre_rh.php">Consultation des offres</a>
+        <div class='collapse navbar-collapse' id='navbarsExample05'>
+          <ul class='navbar-nav mr-auto'>
+            <li class='nav-item'>
+              <a class='nav-link' href='consultation_offre_rh.php?langue=fr'>$str[2]</a>
             </li>
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Création des offres</a>
+            <li class='nav-item active'>
+              <a class='nav-link' href='creation_offre.php?langue=fr'>$str[3]</a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Réception des candidatures</a>
+            <li class='nav-item'>
+              <a class='nav-link' href='#'>$str[4]</a>
             </li>
           </ul>
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Langue</a>
-              <div class="dropdown-menu" aria-labelledby="dropdown05">
-                <a class="dropdown-item" href="#">Français</a>
-                <a class="dropdown-item" href="#">English</a>
+          <ul class='navbar-nav ml-auto'>
+            <li class='nav-item dropdown'>
+              <a class='nav-link dropdown-toggle' data-toggle='dropdown' aria-haspopup='true' aria-expanded='false'>$str[5]</a>
+              <div class='dropdown-menu' aria-labelledby='dropdown05'>
+                <a class='dropdown-item' href='creation_offre.php?langue=fr'>$str[6]</a>
+                <a class='dropdown-item' href='creation_offre.php?langue=en'>$str[7]</a>
               </div>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Mon Compte</a>
+            <li class='nav-item'>
+              <a class='nav-link' href='#'>$str[8]</a>
             </li>
           </ul>
         </div>
-      </nav>
-        <div class="container">
-        <h1>Création de l'offre </h1>
-          <form action="envois/creer_offre_envois.php" method="post">
+      </nav>";
+      ?>
+      <?php
+        echo"
+        <div class='container'>
+        <h1>$str[18]</h1>
+          <form action='envois/creer_offre_envois.php' method='post'>
               <!-- Saisie de l'Intitulé du post-->
-              <div class="form-group">
-                 <label for="nom_post">Intitulé du poste :</label>
-                  <input type="text" class="form-control" name="nom_post" value="" required="required"/>
+              <div class='form-group'>
+                 <label for='nom_post'>$str[19] :</label>
+                  <input type='text' class='form-control' name='nom_post' value='' required='required'/>
               </div>
 
               <!-- Description du poste -->
-              <div class="form-group">
-                <label for="description">Description :</label>
-                <textarea name="description" class="form-control" rows=4 cols=40 required="required"></textarea>
+              <div class='form-group'>
+                <label for='description'>$str[20] :</label>
+                <textarea name='description' class='form-control' rows=4 cols=40 required='required'></textarea>
               </div>
 
               <!-- Saisie du type post-->
-              <div class="form-group">
-                <label for="contrat">Type de l'offre :</label><br />
-                <input type="radio" name="contrat" value="CDI" checked />
+              <div class='form-group'>
+                <label for='contrat'>$str[21] :</label><br />
+                <input type='radio' name='contrat' value=$str[22] checked />
                     CDI
-                <input type="radio" name="contrat" value="CDD" />
+                <input type='radio' name='contrat' value=$str[23] />
                     CDD
-                <input type="radio" name="contrat" value="Interim" />
+                <input type='radio' name='contrat' value=$str[24] />
                     Intérim
               </div>
 
               <!-- Saisie Compétences-->
-              <div class="form-group">
-                <label for="competences">Compétences :</label>
-                <h6> Appuyer sur le bouton ctrl (sur Windows) ou Command (Mac) pour sélectionner plusieurs compétences</h6>
-                <select class="form-control" name="competences[]" multiple>
-                  <?php
+              <div class='form-group'>
+                <label for='competences'>$str[25] :</label>
+                <h6>$str[26]</h6>
+                <select class='form-control' name='competences[]' multiple>";
+                ?>
+
+                <?php
                   include 'bdd/bdd.php';
-                  $requete = "SELECT id_competence, libelle FROM competence;";
+                  $requete = 'SELECT id_competence, libelle FROM competence;';
                   $resultat = mysqli_query($connexion, $requete);
-                  while ($ligne = mysqli_fetch_array($resultat, MYSQLI_BOTH)){ ?>
-                      <option value="<?php echo $ligne['id_competence']; ?>"><?php echo $ligne['libelle']; ?></option>';
+                  while ($ligne = mysqli_fetch_array($resultat, MYSQLI_BOTH)){?>
+                      <option value=<?php echo $ligne['id_competence']; ?>><?php echo $ligne['libelle']; ?></option>';
                   <?php
                   }
                   ?>
-                </select>
+
+                <?php
+                echo "</select>
                 <br />
-                <input type="submit" class="btn btn-secondary" name="ajouter" value="Ajouter"/>
-                <<input type="text" class="form-control" name="nom_post" value="" required="required"/>
+                <input type='submit' class='btn btn-secondary' name='ajouter' value='$str[27]'/>
+                <input type='text' class='form-control' name='nom_post' value='' required='required'/>
               </div>
 
               <!-- A FAIRE Afficher les compétences dans une textarea-->
@@ -92,43 +108,44 @@
 
               ?>
               <!-- Saisie le lieu-->
-              <div class="form-group">
-                <label for="lieu">Lieu :</label>
-                  <input type="text" class="form-control" name="lieu" required="required" value="" />
+              <div class='form-group'>
+                <label for='lieu'>$str[28] :</label>
+                  <input type='text' class='form-control' name='lieu' required='required' value='' />
               </div>
 
               <!-- Saisie du salaire-->
-              <div class="form-group">
-                <label for="salaire">Salaire annuel :</label>
-                  <input type="number" class="form-control" name="salaire" required="required" value="" max="10" />
+              <div class='form-group'>
+                <label for='salaire'>$str[29] :</label>
+                  <input type='number' class='form-control' name='salaire' required='required' value='' max='10' />
               </div>
 
               <!-- Saisie de la date limite de dépots-->
-              <div class="form-group">
-                <label for="date">Date limite de dépots :</label>
-                <input type="date" class="form-control" name="date_depots" required="required" >
+              <div class='form-group'>
+                <label for='date'>$str[30] :</label>
+                <input type='date' class='form-control' name='date_depots' required='required' >
               </div>
 
               <!-- Saisie documents a fournir (cases à cocher)-->
-              <div class="form-group">
-                <label for="documents">Documents à fournir :</label><br />
-                <input type="checkbox" name="doc_cv" />CV
-                <input type="checkbox" name="doc_lm" />Lettre motivation
-                <input type="checkbox" name="doc_video" />Vidéo
+              <div class='form-group'>
+                <label for='documents'>$str[31] :</label><br />
+                <input type='checkbox' name='doc_cv' />$str[32]
+                <input type='checkbox' name='doc_lm' />$str[33]
+                <input type='checkbox' name='doc_video' />$str[34]
               </div>
 
               <!-- Saisie vidéo en url-->
-              <div class="form-group">
-                <label for="url">URL de la Vidéo :</label>
-                <input type="url" class="form-control" name="url" required="required" value="" />
+              <div class='form-group'>
+                <label for='url'>$str[35] :</label>
+                <input type='url' class='form-control' name='url' required='required' value='' />
               </div>
               <!-- Bouton-->
               <div>
-                <input type="submit" class="btn btn-primary" name="soumettre" value="Créer l'offre"/>
+                <input type='submit' class='btn btn-primary' name='soumettre' value=$str[36]/>
               </div>
         </div>
         </form>
-      </div>
+      </div>";
+     ?>
     </body>
     <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
---
+++ langue_fr.php
@@ -0,0 +1,52 @@
+<?php
+// pour consultation_offre_rh
+//menu
+$str[1]="Accueil";
+$str[2]="Consulter des offres";
+$str[3]="Créer des offres";
+$str[4]="Voir des candidatures";
+$str[5]="Langue";
+$str[6]="Français";
+$str[7]="Anglais";
+$str[8]="Mon Compte";
+
+//box
+$str[9]="Lieu";
+$str[10]="Type de contrat";
+$str[11]="Salaire annuel en euros";
+$str[12]="Date limite";
+$str[13]="URL de la vidéo";
+$str[14]="Compétences";
+
+//boutons
+$str[15]="Modifier";
+$str[16]="Voir des candidatures";
+$str[17]="Supprimer";
+
+//création des offres d'emplois
+$str[18]="Création de l'offre";
+$str[19]="Intitulé du poste";
+$str[20]="Description";
+$str[21]="Type de l'offre";
+$str[22]="CDI";
+$str[23]="CDD";
+$str[24]="Intérim";
+$str[25]="Compétences";
+$str[26]=" Appuyer sur le bouton ctrl (sur Windows) ou Command (Mac) pour sélectionner plusieurs compétences";
+$str[27]="Ajouter";
+$str[28]="Lieu";
+$str[29]="Salaire annuel";
+$str[30]="Date limite de dépots";
+$str[31]="Documents à fournir";
+$str[32]="CV";
+$str[33]="Lettre motivation";
+$str[34]="Vidéo";
+$str[35]="URL de la Vidéo";
+$str[36]="Créer l'offre";
+
+
+
+
+
+
+ ?>
