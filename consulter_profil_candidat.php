0f903df1ed58ac935c8a407372d62ddd25980d0f
Amandine <amande038@gmail.com>
2018-11-30 17:35:59 +0100

    reste le bouton a condfigurer

--- consulter_profil_candidat.php
+++ consulter_profil_candidat.php
@@ -48,77 +48,100 @@
       </nav>";
 
       ?>
+      <div class='container'>
+        <div class='row'>
+          <div class='col-lg-12'>
+            <table class="table table-striped">
+              <thead class="thead-dark">
+                <tr>
+                  <th scope="col">Reçu :</th>
+                  <th scope="col">Offre :</th>
+                  <th scope="col">Date limite :</th>
+                  <th scope="col">Salaire</th>
+                  <th scope="col">Type de contrat</th>
+                  <th scope="col">CV :</th>
+                  <th scope="col">Lettre de motivation :</th>
+                  <th scope="col">Vidéo :</th>
 
-      <table class="table table-striped">
-        <thead class="thead-dark">
-          <tr>
-            <th scope="col">Offre :</th>
-            <th scope="col">Date limite :</th>
-            <th scope="col">Salaire</th>
-            <th scope="col">Type de contrat</th>
-            <th scope="col">CV :</th>
-            <th scope="col">Lettre de motivation :</th>
-            <th scope="col">Vidéo :</th>
-            <th scope="col">Reçu :</th>
 
-          </tr>
-        </thead>
-        <tbody>
+                </tr>
+              </thead>
+            <tbody>
+
+
       <!--  les offres avec sa fiche -->
       <?php
-      //afficher en colone ou ligne CV lm et video
       $id_perso=1;
       $id=-1;
       $i=0;
       include 'bdd/bdd.php';
 
-      $_SESSION["Candidat"]=1;
-      if(isset($_SESSION["Candidat"])){
-        $id_personne = $_SESSION["Candidat"];
+      $_SESSION["id_personne"]=1;
+      if(isset($_SESSION["id_personne"])){
+        $id_perso = $_SESSION["id_personne"];
+
+        $resultat= mysqli_query($connexion,"SELECT offre_emplois.id_offre,offre_emplois.libelle, offre_emplois.type_contrat, offre_emplois.salaire, offre_emplois.date_limite, DE.url
+                                            FROM offre_emplois
+                                            INNER JOIN necessite N ON N.id_offre=offre_emplois.id_offre
+                                            INNER JOIN docs D ON D.id= N.id
+                                            INNER JOIN deposer DE ON DE.id=D.id
+                                            INNER JOIN candidature C ON C.id_candidature= DE.id_candidature
+                                            INNER JOIN candidat CA ON CA.id_personne=C.id_personne
+                                            WHERE offre_emplois.id_offre = $id_perso AND CA.id_personne=$id_perso
+                                            ORDER BY offre_emplois.id_offre;" );
+        while($ligne = mysqli_fetch_array($resultat, MYSQLI_BOTH)){
+          $id_offre=$ligne['id_offre'];
+          if ($id_offre!=$id){
+            // if($id!=-1){
+            //
+            // }
+            // affiche l'offre
+
+            $libelle = $ligne['libelle'];
+            $typecontr=$ligne['type_contrat'];
+            $salaire= $ligne['salaire'];
+            $date= $ligne['date_limite'];
+            $url= $ligne['url'];
+            echo "<tr>
+
+                    <td><div class='container'>
+                        <button type='button' class='btn btn-primary disabled'>Vue</button>
+                        </div>
+                    </td>
+                    <td>$libelle</td>
+                    <td>$date</td>
+                    <td>$salaire</td>
+                    <td>$typecontr</td>
+                    <td>$url</td>
+
+                 ";
+
+
+
+          }
+          else{
+            $url=$ligne ['url'];
+            echo"<td>- $url</td>
+            ";
+
+          }
+          $id=$id_offre;
+
+      }
 
       }
 
 
 
-      $resultat= mysqli_query($connexion,"SELECT offre_emplois.id_offre,offre_emplois.libelle, offre_emplois.type_contrat, offre_emplois.salaire, offre_emplois.date_limite, DE.url
-                                          FROM offre_emplois
-                                          INNER JOIN necessite N ON N.id_offre=offre_emplois.id_offre
-                                          INNER JOIN docs D ON D.id= N.id
-                                          INNER JOIN deposer DE ON DE.id=D.id
-                                          INNER JOIN candidature C ON C.id_candidature= DE.id_candidature
-                                          INNER JOIN candidat CA ON CA.id_personne=C.id_personne
-                                          WHERE CA.id_personne = $id_personne
-                                          ORDER BY offre_emplois.id_offre;" );
-      while($ligne = mysqli_fetch_array($resultat, MYSQLI_BOTH)){
-        // affiche l'offre
-
-        $libelle = $ligne['libelle'];
-        $typecontr=$ligne['type_contrat'];
-        $salaire= $ligne['salaire'];
-        $date= $ligne['date_limite'];
-        $url= $ligne['url'];
-        echo "<tr>
-                <td>$libelle</td>
-                <td>$date</td>
-                <td>$salaire</td>
-                <td>$typecontr</td>
-                <td>$url</td>
-                <td><div class='container'>
-                    <button type='button' class='btn btn-primary disabled'>Vue</button>
-                    <button type='button' class='btn btn-primary disabled'>Non Vue</button>
-                    </div>
-                </td>
-             </tr>";
-    }
-
 
 
        ?>
+     </tr>
      </tbody>
    </table>
-<script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
-<script src="bootstrap/js/bootstrap.min.js"></script>
-
-
+ </div>
+</div>
   </body>
+  <script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
 </html>
